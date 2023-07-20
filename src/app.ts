d32bc50c80a50e3e73bd0f7ce15a26ae3b36f920
Alexandr Gerasimov <phantom6391@yandex.ru>
2023-07-20 18:22:17 +0400

    Запуск сервера и создание модели

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import mongoose from "mongoose";
+
+
+const app = express()
+const {PORT=3000}= process.env
+
+mongoose.connect('mongodb://localhost:27017/mestodb')
+
+app.listen(PORT, ()=>{
+  console.log(`Сервер запущен на ${PORT} порту`)
+})
